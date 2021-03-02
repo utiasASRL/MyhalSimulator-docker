3811308cdafeb9e640ea3dd3f4153868be333541
HuguesTHOMAS <huguesthomas218@gmail.com>
2021-03-02 06:05:44 -0500

    .

---
+++ ROS-Dockerfiles/docker_ros_noetic2/docker_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+username=$USER
+userid=$UID
+
+echo $username
+echo $userid
+
+echo ""
+echo "Building image docker_ros_box"
+echo ""
+
+docker image build --build-arg username0=$username \
+--build-arg userid0=$userid \
+--shm-size=64g -t \
+docker_ros_noetic2_$username .
